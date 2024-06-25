859d769fad473d8ba82ac1ca357a586d43e1934a
Helder Sérvio <heldersrvio@gmail.com>
2024-06-24 20:51:00 -0500

    :speech_balloon: Update prices

--- src/components/modals/SubscribeModal.js
+++ src/components/modals/SubscribeModal.js
@@ -11,8 +11,8 @@ import Subscription from '../Subscription';
 import SubscriptionContext from '../../contexts/SubscriptionContext';
 
 const FREE_TRIAL_DAYS = 3;
-const PRICE_ANDROID = 4.15;
-const PRICE_IOS = 4.15;
+const PRICE_ANDROID = 5.99;
+const PRICE_IOS = 6.99;
 
 const SubscribeModal = (props) => {
 	const [isLoading, setIsLoading] = useState(false);
